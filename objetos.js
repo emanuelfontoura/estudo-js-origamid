2d4eede94ca0a5d68664db4b4c22d8fdc42b292d
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-01-16 08:38:56 -0300

    Create objetos.js

---
+++ objetos.js
@@ -0,0 +1,15 @@
+// OBJETO //
+const livro = {
+    nome: "O senhor dos anéis",
+    ano: 1954,
+    autor: "J. R. R. Tolkien",
+} 
+
+// ACESSANDO UMA PROPRIEDADE DO OBJETO //
+console.log(2023 - livro.ano)
+
+// TUDO EM JS É UM OBJETO, UMA SIMPLES VARIÁVEL CRIADA, TORNA-SE UM OBJETO DENTRO DO JS COM VÁRIAS PROPRIEDADES E METÓDOS //
+
+const teste = livro.autor.toUpperCase() // ACESSANDO UMA PROPRIEDADE DO OBJETO LIVRO (autor) E APLICANDO UM METÓDO STRING DESSA PROPRIEDADE (toUpperCase()) //
+
+console.log(teste)
