2a98a3a07ae26cc9905c9d760fea3429793317b7
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-01-20 08:56:16 -0300

    estudos

---
+++ arrays_e_loops.js
@@ -0,0 +1,5 @@
+var lista = ["Javascript", "HTML", "CSS"]
+
+for (var c = 0; c < lista.length; c++){
+    console.log(lista[c])
+}
---
+++ loops_forEach.js
@@ -0,0 +1,7 @@
+const lista = ["JavaScript", "HTML", "CSS"]
+
+function logarItems (item, index){
+    console.log(`Item ${item} de index ${index}`)
+}
+
+lista.forEach(logarItems)
