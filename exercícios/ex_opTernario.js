d8c46614a1899bbd9d1ef649ec2faab96ce889dd
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-02-02 15:03:42 -0300

    estudos + prática

---
+++ exercícios/ex_opTernario.js
@@ -0,0 +1,6 @@
+var possuiCarro = true
+var possuiCasa = true
+
+var darCredito = (possuiCarro && possuiCasa) ? true : false
+
+console.log(darCredito)
---
+++ opTernario.js
@@ -0,0 +1,8 @@
+var idade = 19
+var diabetes = false
+
+var podeBeber = (idade >= 18 && !diabetes)?'Pode beber!': 'Não pode beber!'
+
+console.log(podeBeber)
+
+// (CONDIÇÃO) ? VALOR SE A CONDIÇÃO DER TRUE : VALOR SE A CONDIÇÃO DER FALSE
