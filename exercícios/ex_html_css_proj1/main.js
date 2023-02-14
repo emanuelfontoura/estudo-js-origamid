b1f50d990315063df3a146118c34d2a8a46fa6bb
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-02-14 09:01:21 -0300

    estudos

--- exercícios/ex_html_css_proj1/main.js
+++ exercícios/ex_html_css_proj1/main.js
@@ -13,12 +13,46 @@ console.log(animaisImagens[0].getAttribute('src'))
 console.log(animaisImagens[0].offsetTop)
 
 // EXERCÍCIO 2 (SOMAR O WIDTH DE CADA FOTO)
-var somaWidth = 0
+function somarImagens(){
+    let somaWidth = 0
+    animaisImagens.forEach((item) => {
+        somaWidth += item.offsetWidth
+    })
 
-animaisImagens.forEach((item) => {
-    somaWidth += item.clientWidth
-})
+    console.log(somaWidth)
+}
 
-console.log(somaWidth)
+window.onload = function(){
+    somarImagens()
+}
 
 // EXERCÍCIO 3
+const links = Array.from(window.document.querySelectorAll('.menu-lista > li > a'))
+
+const listaLinksCorretos = []
+const listaLinksIncorretos = []
+
+links.forEach((link) => {
+    if (link.offsetWidth >= 48 && link.offsetHeight >= 48){
+        listaLinksCorretos.push(link.getAttribute('href'))
+    }else{
+        listaLinksIncorretos.push(link.getAttribute('href'))        
+    }
+})
+
+console.log(listaLinksCorretos)
+console.log(listaLinksIncorretos)
+
+// EXERCÍCIO 4
+
+const menu = window.document.querySelector('.menu-lista')
+
+window.addEventListener('resize', () => {
+    let mobile = window.matchMedia('(max-width: 720px)').matches
+    if (mobile){
+        menu.classList.add('menu-mobile')
+    }else{
+        menu.classList.remove('menu-mobile')
+    }
+})
+    
