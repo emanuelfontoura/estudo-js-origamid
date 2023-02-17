3e51bb8563c6efa14349d596772149bd231eaba0
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-02-17 09:06:22 -0300

    estudos

--- exercícios/ex_html_css_proj1/main.js
+++ exercícios/ex_html_css_proj1/main.js
@@ -1,6 +1,8 @@
 const animaisImagens = Array.from(window.document.querySelectorAll('div.animais-imagens-container > img'))
 const animaisConteudo = Array.from(window.document.querySelectorAll('div.animais-texto-container > div'))
   
+// FUNÇÃO DE MOSTRAR O CONTEÚDO DE ACORDO COM A IMAGEM CLICADA
+
 for (let i = 0; i < animaisImagens.length; i++){
     animaisImagens[i].addEventListener('click', () => { // se eu clicar na imagem 3, o "i" assume o valor 3, isso ocorre pois como já foi percorrida a array, o manipulador de eventos associa o número da imagem a variavel "i" quando clicada.
         for (let j = 0; j < animaisConteudo.length; j++){
@@ -13,51 +15,42 @@ for (let i = 0; i < animaisImagens.length; i++){
     })
 }
 
-//EXERCÍCIO 1 (DISTÂNCIA DA PRIMEIRA IMAGEM PARA O TOPO)
-console.log(animaisImagens[0].getAttribute('src'))
-console.log(animaisImagens[0].offsetTop)
+// ANIMAÇÃO DE SELECIONADO NO MENU
 
-// EXERCÍCIO 2 (SOMAR O WIDTH DE CADA FOTO)
-function somarImagens(){
-    let somaWidth = 0
-    animaisImagens.forEach((item) => {
-        somaWidth += item.offsetWidth
+const linksInternos = document.querySelectorAll('a[href^="#"], a[href^="http"]')
+
+function handleLink(event){
+    event.preventDefault()
+    linksInternos.forEach((link) => {
+        link.classList.remove('on')
+        link.classList.add('off')
     })
-
-    console.log(somaWidth)
+    event.currentTarget.classList.add('on')
+    event.currentTarget.classList.remove('off')
 }
 
-window.onload = function(){
-    somarImagens()
+linksInternos.forEach((item) => {
+    item.addEventListener('click', handleLink)
+})
+
+// EXERCICIO
+
+const allElements = document.querySelectorAll('body *')
+
+function elementClick(event){
+    // event.currentTarget.remove()
 }
 
-// EXERCÍCIO 3
-const links = Array.from(window.document.querySelectorAll('.menu-lista > li > a'))
-
-const listaLinksCorretos = []
-const listaLinksIncorretos = []
-
-links.forEach((link) => {
-    if (link.offsetWidth >= 48 && link.offsetHeight >= 48){
-        listaLinksCorretos.push(link.getAttribute('href'))
-    }else{
-        listaLinksIncorretos.push(link.getAttribute('href'))        
-    }
+allElements.forEach((element) => {
+    element.addEventListener('click', elementClick)
 })
 
-console.log(listaLinksCorretos)
-console.log(listaLinksIncorretos)
+// EXERCICIO
 
-// EXERCÍCIO 4
-
-const menu = window.document.querySelector('.menu-lista')
-
-window.addEventListener('resize', () => {
-    let mobile = window.matchMedia('(max-width: 720px)').matches
-    if (mobile){
-        menu.classList.add('menu-mobile')
-    }else{
-        menu.classList.remove('menu-mobile')
+function aumentarFonte(event){
+    if (event.key === "t"){
+        document.documentElement.classList.toggle('textomaior')
     }
-})
-    
+}
+
+window.addEventListener('keydown', aumentarFonte)
