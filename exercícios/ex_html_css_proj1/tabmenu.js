12db65ce02d05a8957aa028a10163062684a6040
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-02-28 08:59:04 -0300

    estudos

--- exercícios/ex_html_css_proj1/tabmenu.js
+++ exercícios/ex_html_css_proj1/tabmenu.js
@@ -1,14 +1,19 @@
 const tabImagens = document.querySelectorAll('.js-tabmenu > img')
 const tabConteudo = document.querySelectorAll('.js-tabconteudo > div')
 
-tabImagens.forEach((itemImagens, indexImagens) => {
-    itemImagens.addEventListener('click', () => {
-        tabConteudo.forEach((itemConteudo, indexConteudo) => {
-            if (indexImagens === indexConteudo){
-                itemConteudo.classList.add('ativar')
-            }else{
-                itemConteudo.classList.remove('ativar')
-            }
+if (tabConteudo.length && tabImagens.length){
+    tabConteudo[0].classList.add('ativar')
+
+    function activeTab (index){
+        tabConteudo.forEach((conteudoItem) => {
+            conteudoItem.classList.remove('ativar')
+        })
+        tabConteudo[index].classList.add('ativar')
+    }
+
+    tabImagens.forEach((imagemItem, imagemIndex) => {
+        imagemItem.addEventListener('click', () => {
+            activeTab(imagemIndex)
         })
     })
-})
+}
