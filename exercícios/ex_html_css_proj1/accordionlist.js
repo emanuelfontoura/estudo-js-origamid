7c5d1cc517aeb3a8a6a585dfe7d1aa348fb0a126
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-03-13 10:55:58 -0300

    estudos

--- exercícios/ex_html_css_proj1/accordionlist.js
+++ exercícios/ex_html_css_proj1/accordionlist.js
@@ -1,12 +1,19 @@
-const perguntas = document.querySelectorAll('.faq-lista > dt')
+function initAccordion(){
+    const perguntas = document.querySelectorAll('.faq-lista > dt')
+    const activeClass = 'ativo'
+    const activeAccordionClass = 'ativoAccordion'
 
-const respostas = document.querySelectorAll('.faq-lista > dd')
+    if (perguntas.length){
+        perguntas[0].classList.add(activeClass)
+        perguntas[0].nextElementSibling.classList.add(activeAccordionClass)
 
-function activeAccordion(){
-    this.classList.toggle('ativar')
-    this.nextElementSibling.classList.toggle('ativar')
+        function activeAccordion(){
+            this.classList.toggle(activeClass)
+            this.nextElementSibling.classList.toggle(activeAccordionClass)
+        }
+
+        perguntas.forEach((dt) => {
+            dt.addEventListener('click', activeAccordion)
+        })
+    }
 }
-
-perguntas.forEach((perguntaItem) => {
-    perguntaItem.addEventListener('click', activeAccordion)
-})
