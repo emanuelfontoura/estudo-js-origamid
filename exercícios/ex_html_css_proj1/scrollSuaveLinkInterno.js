095e23edb43639a622e36345106407a8b6644c23
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-03-15 10:55:56 -0300

    estudos

--- exercícios/ex_html_css_proj1/scrollSuaveLinkInterno.js
+++ exercícios/ex_html_css_proj1/scrollSuaveLinkInterno.js
@@ -3,8 +3,23 @@ const linksInternos2 = document.querySelectorAll('.js-menu a[href^="#"]')
 function scrollToSection (event) {
     event.preventDefault
     const href = event.currentTarget.getAttribute('href')
+
+    // FORMA 1
+
+    // const sectionTop = document.querySelector(href).offsetTop
+    // window.scrollTo({
+    //     top: sectionTop,
+    //     behavior: 'smooth',
+    // })
+
+    // FORMA 2
+
     const section = document.querySelector(href)
-    console.log(section)
+
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    })
 }
 
 linksInternos2.forEach((link) => {
