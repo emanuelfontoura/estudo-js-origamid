9de08534c0b88ebd6686aa33489fa11c157e5354
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-03-28 10:52:12 -0300

    estudos

--- constructor functions/constructFunct/prototype/prototype.js
+++ constructor functions/constructFunct/prototype/prototype.js
@@ -3,6 +3,9 @@ function Person(name, age){
    this.age = age
 }
 
+// CRIAR MÉTODOS DIRETAMENTE NO PROTOTYPE
+// ISSO EVITA COM QUE SEJA CRIADO TUDO DE NOVO AO INSTANCIAR UM NOVO OBJETO
+
 Person.prototype.andar = function (){
     return this.name + ' andou!'
 }
@@ -17,5 +20,24 @@ Person.prototype.abracar = function(){
 
 const emanuel = new Person('Emanuel', 19)
 
-const pais = 'Brasil'
-const numero = 100
+// ------------------------------
+
+// AMBOS TEM O MESMO RESULTADO
+const lista = document.querySelectorAll('li')
+
+    // ACESSA UM MÉTODO DECLARADO NO PROTOTYPE DA CONSTRUCTOR FUNCTION
+const newListaPrototype = Array.prototype.slice.call(lista)
+
+    // ACESSA UM MÉTODO DECLARADO DIRETAMENTE NA CONSTRUCTOR FUNCTION
+const newListaObject = Array.from(lista)
+
+// ------------------------------
+
+// ACESSAR O NOME DAS PROPRIEDADES E MÉTODOS DOS ELEMENTOS OBJETOS
+console.log(Object.getOwnPropertyNames(Person))
+console.log(Object.getOwnPropertyNames(emanuel.__proto__))
+
+// RETORNAR O NOME DO CONSTRUTOR DE UM ELEMENTO
+console.log(emanuel.constructor)
+
+const lista2 = [1, 2, 3]
