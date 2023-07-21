dfcc89c278b2f244666c5f9d247cebec8b67fee1
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-07-20 22:46:11 -0300

    estudos promises

--- jsAssincrono/promises/script.js
+++ jsAssincrono/promises/script.js
@@ -34,18 +34,70 @@ const promessa3 = new Promise((resolve, reject) => {
 
 promessa3.then(resolucao => console.log(resolucao + ' OLÁ'))
 
-// ENCADEAMENTO DE PROMISES
+const retorno = promessa3.then(resolucao => resolucao)
+console.log(retorno)
+
+// ENCADEAMENTO DE PROMISES E USO DO CATCH
 
 const promessa4 = new Promise((resolve, reject) => {
-    resolve('Etapa 1')
+    let condicao = false
+    if(condicao){
+        resolve({nome: 'Emanuel', idade: 18})
+    }else{
+        reject(Error('Ocorreu um erro na promise.'))
+    }
 })
 
-promessa4.then(arg => {
-    console.log(arg)
-    return 'Etapa 2'
-}).then(arg2 => {
+const retorno2 = promessa4.then(arg => {
+    arg.profissao = 'Desenvolvedor'
+    return arg
+})
+.then(arg2 => {
+    arg2.sexo = 'M'
     console.log(arg2)
-    return 'Etapa 3'
-}).then(arg3 => {
-    return arg3 + 4
-}).then(arg4 => console.log(arg4))
+    return arg2
+})
+.catch(rejeitada => {
+    console.log(rejeitada)
+})
+
+// USO DO THEN(RESOLVE, REJECT) E DO FINALLY
+const promessa5 = new Promise((resolve, reject) => {
+    let condicao = false
+    if(condicao){
+        resolve('ACERTO!')
+    }else{
+        reject(Error('ERRO'))
+    }
+})
+
+promessa5.then(resolucao => {
+    console.log('Tudo certo!')
+}, reject => {
+    console.log('Um erro ocorreu.')
+})
+.finally(() => {
+    console.log('A promise foi terminada.')
+})
+
+// PROMISE.ALL()
+const login = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Usuário logado.')
+    }, 1000)
+})
+
+const dados = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Dados carregados.')
+    }, 1500)
+})
+
+const tudoCarregado = Promise.all([login, dados])
+
+tudoCarregado.then(resolucao => console.log(resolucao))
+
+// PROMISE.RACE()
+const primeiroCarregado = Promise.race([login, dados])
+
+primeiroCarregado.then(resolucao => console.log(resolucao))
