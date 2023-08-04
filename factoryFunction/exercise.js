ff7859b041c523f56fcacfbba243bcd03ee2366a
Emanuel Fontoura <emanuelfontoura03@gmail.com>
2023-08-04 20:01:16 -0300

    prática sobre factory functions

---
+++ factoryFunction/exercise.js
@@ -0,0 +1,50 @@
+function $$(selectedElements){
+    const elements = document.querySelectorAll(selectedElements)
+    function hide(){
+        elements.forEach(element => {
+            element.style.display = 'none'
+        })
+        return this // COLOCA-SE ESSES RETURNS PARA PODER REALIZAR ENCADEAMENTOS
+    }    
+    function show(){
+        elements.forEach(element => {
+            element.style.display = 'initial'
+        })
+        return this
+    }  
+    function on(onEvent, callback){
+        elements.forEach(element => {
+            element.addEventListener(onEvent, callback)
+        })
+        return this 
+    }
+    function addClass(className){
+        elements.forEach(element => {
+            element.classList.add(className)
+        })
+        return this
+    }
+    function removeClass(className){
+        elements.forEach(element => {
+            element.classList.remove(className)
+        })
+        return this
+    }
+    return {
+        elements,
+        hide,
+        show,
+        on,
+        addClass,
+        removeClass
+    }
+}
+
+function handleClick(e){
+    console.log('OI')
+}
+
+const li = $$('li')
+li.on('click', handleClick)
+li.addClass('active')
+li.removeClass('active')
